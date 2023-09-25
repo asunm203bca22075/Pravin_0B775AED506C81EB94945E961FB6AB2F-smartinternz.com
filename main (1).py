10925dfc96a0609e38c1f47e829a8db6b7c11618
asunm203bca22075 <144511694+asunm203bca22075@users.noreply.github.com>
2023-09-25 14:10:03 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,22 +1,52 @@
-# Python program to check if year is a leap year or not
-# Python program to check if year is a leap year or not
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance):
+        self._account_number = account_number
+        self._account_holder_name = account_holder_name
+        self._account_balance = initial_balance
 
-year = 2000
+    def deposit(self, amount):
+        if amount > 0:
+            self._account_balance += amount
+            print(f"Deposited ₹ {amount}. New balance: ₹ {self._account_balance}")
+        else:
+            print("Invalid deposit amount.")
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._account_balance:
+            self._account_balance -= amount
+            print(f"Withdrew ₹ {amount}. New balance: ₹ {self._account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient funds.")
 
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+    def display_balance(self):
+        print(f"Account Balance for {self._account_holder_name}:   ₹  {self._account_balance}")
 
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
 
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+# Test the BankAccount class
+account_number = input("Enter account number: ")
+account_holder_name = input("Enter account holder name: ")
+initial_balance = float(input("Enter initial balance: "))
+
+account = BankAccount(account_number, account_holder_name, initial_balance)
+
+while True:
+    print("\nMenu:")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Display Balance")
+    print("4. Exit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        amount = float(input("Enter the deposit amount: "))
+        account.deposit(amount)
+    elif choice == 2:
+        amount = float(input("Enter the withdrawal amount: "))
+        account.withdraw(amount)
+    elif choice == 3:
+        account.display_balance()
+    elif choice == 4:
+        print("Thank you for using the BankAccount program. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please select a valid option.")
